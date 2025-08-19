c30ebfb6398194ec3fdd2fda936524e352651af4
amir-mc <amirfooladi93@gmail.com>
2025-08-19 08:08:20 +0000

    add search query

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,11 +1,74 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
+import { fetchMovies } from '@/services/api'
+import useFetch from '@/services/useFetch'
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from 'react-native'
+import MovieCard from '../components/MovieCard'
+import Searchbar from '../components/searchbar'
 
 const Search = () => {
+  const [searchQuery,setSearchQuery]=useState('')
+    const { data: movie, loading, error,refech:loadMovie,reset } = useFetch(() => fetchMovies({ query: searchQuery }),false);
+    useEffect(()=>{
+      const Timeout =setTimeout(async()=>{
+        if(searchQuery.trim()){
+          await loadMovie()
+        
+      }else{
+        reset()
+      }}, 900);
+      return()=>clearTimeout(Timeout)
+    },[searchQuery])
   return (
-    <View>
-      <Text>search</Text>
-    </View>
+      <View className="flex-1 bg-primary">
+          <Image source={images.bg} className="absolute w-full z-0" />
+          
+          <ScrollView 
+            className="flex-1 px-5" 
+            showsVerticalScrollIndicator={false} 
+            contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}
+          >
+            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+            
+              {loading ? (
+              <ActivityIndicator size='large' color='#000ff' className="mt-10 self-center" />
+            ) : error ? (
+              <Text className="text-white">Server Error: {error.message}</Text>
+            ) : (
+              <View className="flex-1 mt-5 items-center justify-between">
+                <Searchbar
+                  placeholder="search for mooov"
+                  value={searchQuery}
+                  onChangeText={(text:string)=>setSearchQuery(text)}
+                />
+                   {
+                      !loading && !error && searchQuery.trim() && movie?.length > 0  &&(
+                        <Text className='text-xl text-white font-bold'>search result for:{''} 
+                        <Text className='text-accent'>
+                         {searchQuery}
+                        </Text>
+                        </Text>
+                      )
+                    }
+                <>
+                   <Text className="text-lg text-white font-bold ">recently Movie</Text>
+                    <FlatList keyExtractor={(item)=>item.id.toString()} numColumns={3} columnWrapperStyle={{
+                      justifyContent:"flex-start",
+                      gap:20,
+                      paddingRight:5,
+                      marginBottom:10
+                    }} className="mt-2 pb-32 " scrollEnabled={false} data={movie} renderItem={({item})=>(
+                      <MovieCard
+                      {...item}
+                      />
+                    )}/>
+                 
+                </>
+              </View>
+            )}
+          </ScrollView>
+        </View>
   )
 }
 
--- app/components/searchbar.tsx
+++ app/components/searchbar.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { TextInput, View } from 'react-native'
 interface type{
   placeholder:string,
-  onPress?:()=>void
+  onPress?:()=>void,
+  value?:string,
+  onChangeText ?:(text:string)=>void
 }
-const Searchbar = ({placeholder,onPress}:type) => {
+const Searchbar = ({placeholder,onPress,value,onChangeText}:type) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
-      <TextInput placeholder={placeholder} value='' onPress={onPress} onChangeText={()=>{}} placeholderTextColor='#a8b5db'  className='flex-1 ml-2 text-white '></TextInput>
+      <TextInput placeholder={placeholder} value={value} onPress={onPress} onChangeText={onChangeText} placeholderTextColor='#a8b5db'  className='flex-1 ml-2 text-white '></TextInput>
     </View>
   )
 }
