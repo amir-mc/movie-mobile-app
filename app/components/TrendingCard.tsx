5fd20a733b09cb5ad3922d8b96ee37f04ca65490
amir-mc <amirfooladi93@gmail.com>
2025-09-12 09:29:23 +0000

    debug trendig movie

--- app/components/TrendingCard.tsx
+++ app/components/TrendingCard.tsx
@@ -1,30 +1,45 @@
-import { Link } from 'expo-router'
-import React from 'react'
-import { Image, Text, TouchableOpacity } from 'react-native'
+import MaskedView from "@react-native-masked-view/masked-view";
+import { Link } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-const TrendingCard = ({index,movie:{movie_id,title,poster_url}}:TrendingCardProps) => {
+import { images } from "@/constants/images";
+
+const TrendingCard = ({
+  movie: { movie_id, title, poster_url },
+  index,
+}: TrendingCardProps) => {
   return (
-          <Link href={`/movies/${movie_id}`} asChild>
-      <TouchableOpacity className="mr-4 w-32 items-center">
-                            <Image 
-                             source={{uri:poster_url}}
-                             resizeMode='cover'
-                             //className="flex-1  justify-center items-center"
-                             className="w-32 h-48 bg-gray-700 rounded-lg mb-2 overflow-hidden">
-                              
-                             
-                                
-                              </Image>
-                            
-                            <Text 
-                              className="text-white text-center text-xs" 
-                              numberOfLines={1}
-                            >
-                              {title}
-                            </Text>
-                          </TouchableOpacity>
-                        </Link>
-  )
-}
+    <Link href={`/movies/${movie_id}`} asChild>
+      <TouchableOpacity className="w-32 relative pl-5">
+        <Image
+          source={{ uri: poster_url }}
+          className="w-32 h-48 rounded-lg"
+          resizeMode="cover"
+        />
 
-export default TrendingCard
+        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+          <MaskedView
+            maskElement={
+              <Text className="font-bold text-white text-6xl">{index + 1}</Text>
+            }
+          >
+            <Image
+              source={images.rankingGradient}
+              className="size-14"
+              resizeMode="cover"
+            />
+          </MaskedView>
+        </View>
+
+        <Text
+          className="text-sm font-bold mt-2 text-light-200"
+          numberOfLines={2}
+        >
+          {title}
+        </Text>
+      </TouchableOpacity>
+    </Link>
+  );
+};
+
+export default TrendingCard;
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,58 +1,57 @@
 import { Client, Databases, ID, Query } from "react-native-appwrite";
 
-const database_id= process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
-const collection_id= process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
-const client=new Client()
-    .setEndpoint(`${process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!}`)
-    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
-    )
-const databse= new Databases(client)
-export const updateserchCount=async (query:string,movie:Movie)=>{
-    try{
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
 
-        const result=await databse.listDocuments(database_id,collection_id,[
-            
-            Query.equal('searchTerm',query)
-        ])
-    if(result.documents.length>0){
-        const existingMovie= result.documents[0];
-        await databse.updateDocument(
-            database_id,
-            collection_id,
-            existingMovie.$id,{
-                count:existingMovie.count+1
-            }
-        )
-    }else{
-        await databse.createDocument(database_id,collection_id,ID.unique(),{
-            searchTerm:query,
-            movie_id:movie.id,
-            count:1,
-            title:movie.title,
-            poster_url:`https://image.tmdb.org/t/p/w500${movie.poster_path}`
-        })
-    }
-    //console.log(result)
-}catch(error) {
-    console.log(error)
-    throw error
-}
+const database = new Databases(client);
 
-}
-export const gettrendMovie = async (): Promise<TrendingMovie[]|undefined>=>{
-    try { 
-        
-        const result=await databse.listDocuments(database_id,collection_id,[
-            
-            Query.limit(5),
-            Query.orderDesc('count')
-        ])
-        return result.documents as unknown as TrendingMovie[]
-    } catch (error) {
-        if(error){
-            console.log(error)
-            return undefined
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1,
         }
+      );
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm: query,
+        movie_id: movie.id,
+        title: movie.title,
+        count: 1,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
     }
-}
+  } catch (error) {
+    console.error("Error updating search count:", error);
+    throw error;
+  }
+};
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+};
