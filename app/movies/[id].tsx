1efdd235b20d5edb3bfdd022f19f89cde6530135
amir-mc <amirfooladi93@gmail.com>
2025-09-23 12:20:28 +0000

    edit style

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -15,13 +15,13 @@ import useFetch from "@/services/usefetch";
   
 interface MovieInfoProps {
   label: string;
-  value?: string | number | null;
+  value?: string | number | null; 
 }
-
+ 
 const MovieDeatils = ({ label, value }: MovieInfoProps) => (
-  <View className="flex-col items-start justify-center mt-5">
+  <View className="flex-col items-start justify-center mt-5 "> 
     <Text className="text-light-200 font-normal text-sm">{label}</Text>
-    <Text className="text-light-100 font-bold text-sm mt-2">
+    <Text className="text-light-100 font-bold text-sm mt-2"> 
       {value || "N/A"}
     </Text>
   </View>
